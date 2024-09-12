4bd035324ca8b2e9938ce69928855b00630cac27
Damir Nasibullin <damir.nasibullin@sharp-dev.net>
2024-09-12 17:12:20 +0500

    Update submodule ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./@shared/src/components";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    console.log("Click!");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <Button />
+      <h2>Using shared submodule</h2>
+      <Button onClick={handleClick}>Submodule button</Button>
     </>
   );
 }
